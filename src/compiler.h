76b357e881301631f1a8bd579ab95fc956447990
_ <yancyACC0UNT@gmail.com>
2026-06-19 07:50:04 +0800

    first

---
+++ src/compiler.h
@@ -0,0 +1,42 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <iostream>
+#include <string>
+//lex
+enum class TokenType {
+    TK_PRE,
+    TK_KEYWORD,
+    TK_OP,
+    TK_ATTR,
+    TK_IDENT,
+    TK_CHAR,
+    TK_STR,
+    TK_INT,
+    TK_FLOAT,
+    TK_SEMI,
+    TK_LBRACE,
+    TK_RBRACE,
+    TK_EOF,
+};
+struct Token {
+    Token *prev;
+    TokenType type;
+    std::string name;
+    Token *next;
+    union {
+        char ch;
+        char *str;
+        long integer;
+        double floating;
+        char *args[2];
+    } as;
+};
+class Lexer {
+public:
+    Lexer(const char *filename);
+    Token *tokenize();
+    ~Lexer() = default;
+};
+
+#endif//COMPILER_H
